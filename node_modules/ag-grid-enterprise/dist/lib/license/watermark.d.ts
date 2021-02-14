5758d440ab7f7a25430dec088398c22c1dad6b52
davis4acca@gmail.com <davis4acca@gmail.com>
2021-02-14 11:45:57 +0000

    t2-245  with react teemplate created

--- node_modules/ag-grid-enterprise/dist/lib/license/watermark.d.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from 'ag-grid-community';
-import { LicenseManager } from '../licenseManager';
-export declare class WatermarkComp extends Component {
-    licenseManager: LicenseManager;
-    private eLicenseTextRef;
-    constructor();
-    private postConstruct;
-    private shouldDisplayWatermark;
-}
--- node_modules/ag-grid-enterprise/dist/lib/main.d.ts
+++
@@ -1,6 +0,0 @@
-export { EnterpriseCoreModule } from "./agGridEnterpriseModule";
-export { LicenseManager } from "./licenseManager";
-
-export { SetFilterModule } from './setFilterModule';
-export { SetFilter } from './setFilter/setFilter';
-export { SetFilterModel } from './setFilter/setFilterModel';
