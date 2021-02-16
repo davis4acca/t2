d5546c7ddb6318fa1ffb6ca342b9c78a8c67da29
davis4acca@gmail.com <davis4acca@gmail.com>
2021-02-16 09:40:35 +0000

    t2-123  with react teemplate created

--- scripts/t2-create-react.sh
+++ scripts/t2-create-react.sh
@@ -8,7 +8,6 @@
 # Parse arguments
 for ARGUMENT in "$@"
 do
-
     KEY=$(echo $ARGUMENT | cut -f1 -d=)
     VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
 
@@ -23,19 +22,19 @@ cd $T2_HOME
 
 # echo "Creating... TICKET = t2-$TICKET"
 
-# git pull && /
-# npx create-react-app ./projects/t2-$TICKET && /
-# npm i --save ag-grid-react ag-grid-community ag-grid-enterprise && /
+git pull && /
+ create-react-app ./projects/t2-$TICKET && /
+npm i --save ag-grid-react ag-grid-community ag-grid-enterprise && /
 
-# TEMPLATE_FILE="./templates/$TEMPLATE/src"
+TEMPLATE_FILE="./templates/$TEMPLATE/src"
 
-# if test -d "$TEMPLATE_FILE"; then
-#     echo "applying $TEMPLATE template"
-#     'cp' -rf ././templates/$TEMPLATE/*  ./projects/t2-$TICKET/
-# fi
+if test -d "$TEMPLATE_FILE"; then
+    echo "applying $TEMPLATE template"
+    'cp' -rf ././templates/$TEMPLATE/*  ./projects/t2-$TICKET/
+fi
 
 git add . &&/
 git commit -m "t2-$TICKET  with $TEMPLATE teemplate created" &&/
 git push 
 
-echo "DONE"
+echo "DONE"`
