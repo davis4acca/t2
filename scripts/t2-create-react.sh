4080b7404ed8c9626882d843c7d2710eec998a4b
davis4acca@gmail.com <davis4acca@gmail.com>
2021-02-18 09:27:36 +0000

    t2-2452  with  template created

--- scripts/t2-create-react.sh
+++ scripts/t2-create-react.sh
@@ -29,13 +29,13 @@ else
   exit 1
 fi
 
-# TICKET NR ALREADY EXIST = EXIT
+# # TICKET NR ALREADY EXIST = EXIT
 TICKET_PROJECT_PATH="$T2_HOME/projects/t2-$TICKET"
 
-# if test -d "$TICKET_PROJECT_PATH"; then
-#   echo "TICKET ALREADY EXIST"
-#   exit 1
-# fi
+if test -d "$TICKET_PROJECT_PATH"; then
+  echo "TICKET ALREADY EXIST"
+  exit 1
+fi
 
 
 PATH_TO_PROJECT="$T2_HOME""projects/t2-$TICKET"
@@ -45,18 +45,17 @@ echo $PATH_TO_PROJECT
 echo "Creating... TICKET = t2-$TICKET"
 
 git pull && /
-# create-react-app $PATH_TO_PROJECT && /
-# npm i --save ag-grid-react ag-grid-community ag-grid-enterprise --prefix $PATH_TO_PROJECT && /
+create-react-app $PATH_TO_PROJECT && /
+npm i --save ag-grid-react ag-grid-community ag-grid-enterprise --prefix $PATH_TO_PROJECT && /
 
 
 # test if TEMPLATE FOLDER EXIST
 if test -d "$TEMPLATE_FOLDER"; then
     echo "applying $TEMPLATE template"
-    'cp' -rf "$T2_HOME/templates/$TEMPLATE/*"  "$T2_HOME/projects/t2-$TICKET" /
+    'cp' -rf $T2_HOME/templates/$TEMPLATE/*  $T2_HOME/projects/t2-$TICKET
 fi
 
 git add . &&/
 git commit -m "t2-$TICKET  with $TEMPLATE template created" &&/
 git push 
 
-echo "DONE"`
