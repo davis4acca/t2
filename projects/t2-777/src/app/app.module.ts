68ca81af404725f2e4f68e44ecda0303da8246fe
davis4acca@gmail.com <davis4acca@gmail.com>
2021-03-01 09:51:44 +0000

    update

--- projects/t2-777/src/app/app.module.ts
+++ projects/t2-777/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import "core-js/es7/reflect";
 
 
 // ag-grid
