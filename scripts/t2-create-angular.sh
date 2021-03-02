140d73fbfb70f4040c5f88d5df6e9e659e193752
davis4acca@gmail.com <davis4acca@gmail.com>
2021-03-02 07:39:58 +0000

    clear

--- scripts/t2-create-angular.sh
+++ scripts/t2-create-angular.sh
@@ -5,59 +5,57 @@
 # GO TO T2_HOME and PARSE ARGUMENTS
 cd $T2_HOME
 
-for ARGUMENT in "$@"; do
-  KEY=$(echo $ARGUMENT | cut -f1 -d=)
-  VALUE=$(echo $ARGUMENT | cut -f2 -d=)
 
-  case "$KEY" in
-  TICKET) TICKET=${VALUE} ;;
-  TEMPLATE) TEMPLATE=${VALUE} ;;
-  *) ;;
-  esac
-done
+PARSE_TICKET_TEMPLATE_ARGUMENTS_PATH="$T2_HOME/scripts/helpers/parse-ticket-template-arguments.sh"
 
-# # NO TICKET = EXIT
-if [ "$TICKET" ]; then
-  echo "CREATING $TICKET"
-else
-  echo "NO TICKET PROVIDED"
-  exit 1
-fi
+echo "hello" ${PARSE_TICKET_TEMPLATE_ARGUMENTS_PATH}
 
-#  PROJECT WITH THE TICKET NR ALREADY EXIST = EXIT
-TICKET_PROJECT_PATH="$T2_HOME/projects/t2-$TICKET"
+source ${PARSE_TICKET_TEMPLATE_ARGUMENTS_PATH}
 
-if test -d "$TICKET_PROJECT_PATH"; then
-  echo "PROJECT WITH THE TICKET NR ALREADY EXIST"
-  exit 1
-fi
+echo ${TICKET} ${TEMPLATE}
 
-PROJECT_DIR_PATH="$T2_HOME/projects/t2-$TICKET"
-TEMPLATE_DIR_PATH="$T2_HOME/templates/$TEMPLATE"
+# # # NO TICKET = EXIT
+# if [ "$TICKET" ]; then
+#   echo "CREATING $TICKET"
+# else
+#   echo "NO TICKET PROVIDED"
+#   exit 1
+# fi
+
+# #  PROJECT WITH THE TICKET NR ALREADY EXIST = EXIT
+# TICKET_PROJECT_PATH="$T2_HOME/projects/t2-$TICKET"
+
+# if test -d "$TICKET_PROJECT_PATH"; then
+#   echo "PROJECT WITH THE TICKET NR ALREADY EXIST"
+#   exit 1
+# fi
+
+# PROJECT_DIR_PATH="$T2_HOME/projects/t2-$TICKET"
+# TEMPLATE_DIR_PATH="$T2_HOME/templates/$TEMPLATE"
 
 
-git pull && /
+# git pull && /
 
-# ANGULAR SPECIFIC
+# # ANGULAR SPECIFIC
 
-ng new hello --directory "./projects/t2-$TICKET" --style scss --routing false --strict true --skip-git true && /
-npm i --save ag-grid-angular ag-grid-community ag-grid-enterprise --prefix $PROJECT_DIR_PATH && /
+# ng new hello --directory "./projects/t2-$TICKET" --style scss --routing false --strict true --skip-git true && /
+# npm i --save ag-grid-angular ag-grid-community ag-grid-enterprise --prefix $PROJECT_DIR_PATH && /
 
 
-# test if TEMPLATE PROVIDED AND IF TEMPLATE FOLDER EXIST
-# If exist then use that template by copying over files from template to project
-if [ "$TEMPLATE" ]; then
-  if test -d "$TEMPLATE_DIR_PATH"; then
-    echo "applying $TEMPLATE template"
-    'cp' -rf $TEMPLATE_DIR_PATH/* $PROJECT_DIR_PATH
-  fi
-else
-  echo "NO TEMPLATE PROVIDED - creating regular angular project"
-fi
+# # test if TEMPLATE PROVIDED AND IF TEMPLATE FOLDER EXIST
+# # If exist then use that template by copying over files from template to project
+# if [ "$TEMPLATE" ]; then
+#   if test -d "$TEMPLATE_DIR_PATH"; then
+#     echo "applying $TEMPLATE template"
+#     'cp' -rf $TEMPLATE_DIR_PATH/* $PROJECT_DIR_PATH
+#   fi
+# else
+#   echo "NO TEMPLATE PROVIDED - creating regular angular project"
+# fi
 
-git add . && /
-git commit -m "t2-$TICKET  with $TEMPLATE template created" && /
-git push && /
+# git add . && /
+# git commit -m "t2-$TICKET  with $TEMPLATE template created" && /
+# git push && /
 
-echo "complete"
-echo "LINK TO CODESANDBOX: https://codesandbox.io/s/github/davis4acca/t2/tree/main/projects/t2-${TICKET}"
+# echo "complete"
+# echo "LINK TO CODESANDBOX: https://codesandbox.io/s/github/davis4acca/t2/tree/main/projects/t2-${TICKET}"
